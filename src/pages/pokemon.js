e7c4fc3519530405bf95926fd410009cff84ddcd
Martijn Beljaards <martijn_beljaards@hotmail.com>
2020-10-07 00:24:54 +0200

    css added

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -3,14 +3,79 @@ import Link from 'next/link';
 
 export default function Pokemon({ pokemonList }) {
     return (
-        <div>
-            {pokemonList.slice(0, 10).map((e, index) => (
+        <div className="home-container">
+            <div className="home-header">
+                <h2>Pokémon API - Flinndal Assessment</h2>
+                <h3>By: Martijn Beljaards</h3>
+            </div>
+            <div className="home-section">
+                {pokemonList.slice(0, 10).map((e, index) => (
                 <div key={index}>
                     <Link as={`/pokemon/${e.name}`} href="/pokemon/[name]">
-                        <a>{e.name}</a>
+                        <a className="home-pokemon-anchor-link">{e.name}</a>
                     </Link>
                 </div>
-            ))}
+                ))}
+            </div>
+
+            <style jsx>
+            {`
+                body {
+                    color: red;
+                }
+                    
+                .home-container {
+                    font-family: Calibri,Helvetica,sans-serif; 
+                    display: flex;
+                    flex-direction: column;
+                    background-color: rgba(167, 219, 141);
+                    margin: -8px;
+                    padding: 8px;
+                    color: rgba(53, 82, 38);
+                    min-height: 100%;
+                }
+
+                .home-container .home-header {
+                    text-align: center;
+                    outline-style: outset;
+                    outline-color: rgba(78, 130, 52);
+                    background-color: rgba(120, 200, 80);
+                    margin-bottom: 20px;
+                }
+
+                .home-header h2 {
+                    margin-bottom: 0;
+                }
+
+                .home-header h3 {
+                    margin-top: 0;
+                }
+
+                .home-container .home-section {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: center;
+                }
+
+                .home-container .home-section > div {
+                    flex-basis: 25%;
+                    padding: 10px;
+                    margin: 10px;
+                    text-align: center;
+                    outline-style: outset;
+                    outline-color: rgba(78, 130, 52);
+                    background-color: rgba(120, 200, 80);
+                }
+
+                .home-container .home-section .home-pokemon-anchor-link {
+                    display: block;
+                    color: rgba(53, 82, 38);
+                    font-weight: bold;
+                    text-decoration: none;
+                    text-transform: Capitalize;
+                }
+            `}
+            </style>
         </div>
     )
 }
--- src/pages/pokemon/[name].js
+++ src/pages/pokemon/[name].js
@@ -32,6 +32,7 @@ export default function Details({pokemonList}) {
             (element.base_stat !== null) ? stats.push(element.base_stat) : stats.push("--");
         });
     } else {
+        // Values for cases that dont have values in the API (i.e. when you type a pokemon in the url that doesnt exist)
         sprite = "#"; 
         types = ["N/A", "N/A"];
         abilities = ["N/A", "N/A"];
@@ -41,46 +42,121 @@ export default function Details({pokemonList}) {
     }
 
     return (
-        <div>
-        {console.log("POKEMONLIST", pokemonList)}
-            <h2>{router.query.name}</h2>
-            {
-                /*
-                    ==> get all information using an component? => "Cant resolve?"
-                */
-                <div>
-                    <img src={`${sprite}`} alt={`${router.query.name}_sprite`} />
-                    <p><strong>Typing:</strong> {types[0]} {types[1]}</p>
-                    <p><strong>Ability:</strong> {abilities[0]} - <strong>Hidden ability:</strong> {abilities[1]}</p>
-                    <p><strong>Height:</strong> {height} - <strong>Weight:</strong> {weight}</p>
-                    <div>
-                        <strong>Base stats:</strong>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>HP</th>
-                                    <td>{stats[0]}</td>
-                                    <th>Speed</th>
-                                    <td>{stats[5]}</td>
-                                </tr>
-                                <tr>
-                                    <th>Attack</th>
-                                    <td>{stats[1]}</td>
-                                    <th>Sp. Attack</th>
-                                    <td>{stats[3]}</td> 
+        <div className="detail-container">
+            <div className="detail-header">
+                <h2 className="detail-header-title">{router.query.name}</h2>
+            </div>
+            {/* ==> get all information using an component? => "Cant resolve?" */}
+            <div className="detail-sprite-section">
+                <img src={`${sprite}`} className="detail-pokemon-sprite" alt={`${router.query.name}_sprite`}/>
+            </div>
+            <div className="detail-info-section">
+                <p className="detail-pokemon-typing"><strong>Typing:</strong> {types[0]} {types[1]}</p>
+                <p className="detail-pokemon-abilities"><strong>Ability:</strong> {abilities[0]} - <strong>Hidden ability:</strong> {abilities[1]}</p>
+                <p className="detail-pokemon-measures"><strong>Height:</strong> {height} - <strong>Weight:</strong> {weight}</p>
+                <div className="detail-pokemon-stats">
+                    <strong>Base stats:</strong>
+                    <table className="detail-pokemon-stats-table">
+                        <tbody>
+                            <tr>
+                                <th>HP</th>
+                                <td>{stats[0]}</td>
+                                <th>Speed</th>
+                                <td>{stats[5]}</td>
                             </tr>
-                                <tr>
-                                    <th>Defence</th>
-                                    <td>{stats[2]}</td>
-                                    <th>Sp. Defence</th>
-                                    <td>{stats[4]}</td>                        
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                            <tr>
+                                <th>Attack</th>
+                                <td>{stats[1]}</td>
+                                <th>Sp. Attack</th>
+                                <td>{stats[3]}</td> 
+                        </tr>
+                            <tr>
+                                <th>Defence</th>
+                                <td>{stats[2]}</td>
+                                <th>Sp. Defence</th>
+                                <td>{stats[4]}</td>                        
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+            </div>
+            {/* CSS - Lelijk, mid-page... Maar global.css, e.d. werken niet, dit is de enigste manier die ik werkend krijg... */}
+            {/* Body kan ik niet aanpassen? --> 100% min-height voor volledige hoogte van page gebruiken... (body heeft geen position) */}
+        <style jsx>
+        {`
+            .detail-container {
+                font-family: Calibri,Helvetica,sans-serif; 
+                display: flex;
+                flex-direction: column;
+                background-color: rgba(167, 219, 141);
+                margin: -8px;
+                padding: 8px;
             }
-        </div>
+
+            .detail-container .detail-header,
+            .detail-container .detail-sprite-section,
+            .detail-container .detail-info-section {
+                text-align: center;
+                color: rgba(53, 82, 38);
+            }
+
+            .detail-container .detail-header {
+                outline-style: outset;
+                outline-color: rgba(78, 130, 52);
+                background-color: rgba(120, 200, 80);
+            }
+
+            .detail-header .detail-header-title {
+                text-transform: Capitalize;
+            }
+
+            .detail-container .detail-sprite-section {
+
+            }
+
+            .detail-sprite-section .detail-pokemon-sprite {
+
+            }
+
+            .detail-container .detail-info-section {
+                outline-style: inset;
+                outline-color: rgba(78, 130, 52);
+                background-color: rgba(120, 200, 80);
+            }
+
+            .detail-info-section .detail-pokemon-typing,
+            .detail-info-section .detail-pokemon-abilities,
+            .detail-info-section .detail-pokemon-measures {
+                display: block;
+            }
+
+            .detail-pokemon-stats-table {
+                margin: 0 auto;
+                width: 50%;
+                margin-bottom: 10px;
+            }
+
+            .detail-pokemon-stats-table td,
+            .detail-pokemon-stats-table th {
+                border: 1px solid rgba(53, 82, 38);
+            }
+
+            .detail-pokemon-stats-table th {
+                width: 33.3%
+            }
+
+            .detail-pokemon-stats-table td {
+                width: 16.67%
+            }
+
+            @media (max-width: 600px) {
+                .detail-pokemon-stats-table {
+                    width: 80%;
+                }
+            }
+        `}
+        </style>
+    </div>
     );
 }
 
